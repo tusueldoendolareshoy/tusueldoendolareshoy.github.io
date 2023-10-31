a3d8ccee931d3b0725ca9fc87ac8d72d92f610c2
Saulo Cid <145942062+tusueldoendolareshoy@users.noreply.github.com>
2023-10-31 18:34:46 -0300

    Cambio script

--- script/script.js
+++ script/script.js
@@ -1,9 +1,26 @@
-document.getElementById("calcular").addEventListener("click", function() {
+const apiUrl = 'https://api.bluelytics.com.ar/v2/latest';
 
-    var numero1 = parseFloat(document.getElementById("numero1").value);
-    var numero2 = parseFloat(document.getElementById("numero2").value);
+const numero1Input = document.getElementById('numero1');
+const numero2Input = document.getElementById('numero2');
 
-    var producto = (numero1 / numero2).toFixed(2);
+fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+        const valorBlue = data.blue.value_sell;
+        numero2Input.value = valorBlue;
+    })
+    .catch(error => {
+        console.error('Error al cargar los datos:', error);
+    });
 
-    document.getElementById("producto").value = producto;
+document.getElementById("calcular").addEventListener("click", function () {
+    var numero1 = parseFloat(numero1Input.value);
+    var numero2 = parseFloat(numero2Input.value);
+
+    if (!isNaN(numero1) && !isNaN(numero2) && numero2 !== 0) { 
+        var producto = (numero1 / numero2).toFixed(2);
+        document.getElementById("producto").value = producto;
+    } else {
+        alert("Por favor, ingrese valores válidos en ambos campos.");
+    }
 });
