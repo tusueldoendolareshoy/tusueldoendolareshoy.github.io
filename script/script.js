d12c6dd1bd89aec02de8723d6113bfdef41a1a9c
Saulo Cid <118272844+saulocid@users.noreply.github.com>
2024-01-10 16:44:00 -0300

    Cambios API
    Modificamos la fuente de la API, colocamos párrafo haciendo mención y link, cambiamos el contenido del DIV de divisa y mostramos los valores por API

--- script/script.js
+++ script/script.js
@@ -2,12 +2,15 @@ const apiUrl = 'https://api.bluelytics.com.ar/v2/latest';
 
 const numero1Input = document.getElementById('numero1');
 const numero2Input = document.getElementById('numero2');
+const dolarCompra = document.getElementById('dolarCompra');
+const dolarVenta = document.getElementById('dolarVenta');
 
 fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
-        const valorBlue = data.blue.value_sell;
-        numero2Input.value = valorBlue;
+        numero2Input.value = data.blue.value_sell;
+        dolarCompra.textContent = data.blue.value_buy;
+        dolarVenta.textContent = data.blue.value_sell;
     })
     .catch(error => {
         console.error('Error al cargar los datos:', error);
